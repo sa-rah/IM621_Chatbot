c729013488a0345ec1c87fa167b412d679e55059
Sarah Sauseng <sarah.sauseng@gmail.com>
2017-12-28 15:44:36 +0100

    ADD setup part for heroku.

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    telegraf: '513828862:AAF8dcB1-Fzulu6Ax3GAuvHp1So0XwV2dAg',
+    apiai: 'c715ce95287841e088cb160f8cb507ab',
+    unsplash: 'bc971ef99984d2d725007054f7dfef2da022b31c192fd1d27a4440c5330b3662'
+}
---
+++ index.js
@@ -0,0 +1,21 @@
+const Telegraf = require('telegraf');
+const express = require('express');
+const expressApp = express();
+
+const API_TOKEN = process.env.API_TOKEN || '';
+const PORT = process.env.PORT || 3000;
+const URL = process.env.URL || 'https://im621-pixx-bot.herokuapp.com';
+
+const bot = new Telegraf(API_TOKEN);
+bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
+expressApp.use(bot.webhookCallback(`/bot${API_TOKEN}`));
+/*
+ your bot commands and all the other stuff on here ....
+*/
+// and at the end just start server on PORT
+expressApp.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+expressApp.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
